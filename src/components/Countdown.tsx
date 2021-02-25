302c852afb22630655dbc1b6f8a903141c4cda15
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-02-24 23:20:38 -0300

    aula3: finalizando contexto

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Countdown.module.css';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
-    const contextData = useContext(ChallengesContext);
-
-    console.log(contextData)
+    const { startNewChallenge } = useContext(ChallengesContext);
 
     const [time, setTime] = useState(0.1 * 60)
     const [isActive, setIsActive] = useState(false);
@@ -31,6 +29,7 @@ export function Countdown() {
         } else if (isActive && time == 0) {
             setHasFinished(true);
             setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time])
 
