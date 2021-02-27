db949194b29a0f077e3e2172d9b0240ebfb5ac20
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-02-26 21:27:00 -0300

    aula4: countdowncontext integrado ao app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/global.css';
 
 import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { CountDownProvider } from '../contexts/CountdownContext';
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+      <CountDownProvider>
+        <Component {...pageProps} />
+      </CountDownProvider>
     </ChallengesProvider>
   )
 }
